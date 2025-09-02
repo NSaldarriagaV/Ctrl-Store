6b7e8f70307eb8b1989a0b58515195532dc5520a
Eromero1907 <ejromeror@eafit.edu.co>
2025-09-02 16:25:55 -0500

    feat(product): add main_image field and configure media support for product images

--- ctrlstore/urls.py
+++ ctrlstore/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(("ctrlstore.apps.catalog.urls", "catalog"), namespace="catalog")),
     path("auth/", include(("ctrlstore.apps.authx.urls", "authx"), namespace="authx")),
+    path("cart/", include("ctrlstore.apps.cart.urls", namespace="cart")),
+    path("order/", include("ctrlstore.apps.order.urls", namespace="order")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
